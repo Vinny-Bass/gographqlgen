4c7243c016ef89c407ee39e2a082c3468c5ca07e
Vinicius Bass <raylee558@gmail.com>
2023-05-30 16:12:59 +0100

    first commit

---
+++ gographqlgen_test.go
@@ -0,0 +1,49 @@
+package graphqlgen_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/Vinny-Bass/gographqlgen"
+)
+
+type Author struct {
+	Name  string
+	Books []Book
+}
+
+type Book struct {
+	Title  string
+	Author *Author
+}
+
+func TestSchemaGeneration(t *testing.T) {
+
+	authorType := gographqlgen.ParseStruct(Author{})
+	authorSchema := gographqlgen.GenerateSchema(authorType)
+
+	expectedSchema := `
+		type Author {
+			Name: String
+			Books: [Book]
+		}
+	`
+
+	if strings.Join(strings.Fields(authorSchema), "") != strings.Join(strings.Fields(expectedSchema), "") {
+		t.Errorf("Expected:\n%s\nGot:\n%s", expectedSchema, authorSchema)
+	}
+
+	expectedSchema = `
+		type Book {
+			Title: String
+			Author: Author
+		}
+	`
+	bookType := gographqlgen.ParseStruct(Book{})
+	bookSchema := gographqlgen.GenerateSchema(bookType)
+
+	if strings.Join(strings.Fields(bookSchema), "") != strings.Join(strings.Fields(expectedSchema), "") {
+		t.Errorf("Expected:\n%s\nGot:\n%s", expectedSchema, bookSchema)
+	}
+
+}
